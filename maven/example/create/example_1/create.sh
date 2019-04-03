8604f91a89326adb4488f174668cb58ed2464b07
zhongliheng <nightdev@sina.com>
2019-04-03 22:43:58 +0800

    add maven example

---
+++ maven/example/create/example_1/create.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mvn -B archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=com.mycompany.app -DartifactId=my-app
---
+++ maven/example/create/example_2/create.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mvn -B archetype:maven-archetype-j2ee-simple -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=com.mycompany.app -DartifactId=my-app
+
