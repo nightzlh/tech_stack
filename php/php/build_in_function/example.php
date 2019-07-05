f4200e22b48e7779df7461f559e10c51ac19b96a
zhongliheng <zhongliheng@shoyoo.com>
2019-07-05 15:59:55 +0800

    add some example

---
+++ php/php/build_in_function/example.php
@@ -0,0 +1,12 @@
+<?php
+
+	/*$arr = [];
+	$ret = implode($arr);
+	var_dump($ret);*/
+
+	$arr = [0.0, null, 0, null, 5, null, 3];
+	$ret = min($arr);
+	var_dump($ret);
+	echo ($ret);
+
+?>
---
+++ php/php/type_detail/type.php
@@ -0,0 +1,10 @@
+<?php
+	$a = 5;
+	
+	$b = 5;
+
+	$c = (int)($a == $b);
+	
+	var_dump($c);	
+	echo $c;
+?>
