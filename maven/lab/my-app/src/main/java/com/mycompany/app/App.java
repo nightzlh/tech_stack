c6ba7db296b4b584aad53861fdaea214d4254ee9
zhongliheng <zhongliheng@shoyoo.com>
2021-01-28 17:54:17 +0800

    fix:nil add examle for maven get dependence of spring framework

--- maven/lab/my-app/src/main/java/com/mycompany/app/App.java
+++ maven/lab/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,16 @@
 package com.mycompany.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        ApplicationContext context = new ClassPathXmlApplicationContext();
         System.out.println( "Hello World!" );
     }
 }
