6fa4241d7881b11549a382144b2ebe5ddfb84580
zhongliheng <zhongliheng@shoyoo.com>
2021-02-09 17:50:54 +0800

    update bean load in example-ioc

--- upper/spring/lab/example-ioc/src/main/java/night/dev/App.java
+++ upper/spring/lab/example-ioc/src/main/java/night/dev/App.java
@@ -2,12 +2,14 @@ package night.dev;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("file:/Users/zlh/github/tech_stack/upper/spring/lab/example-ioc/Beans.xml");
+        // ApplicationContext context = new ClassPathXmlApplicationContext("file:/Users/zlh/github/tech_stack/upper/spring/lab/example-ioc/Beans.xml");
+        ApplicationContext context = new FileSystemXmlApplicationContext("Beans.xml");
         HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
         objA.setMessage("I'm object A");
         objA.getMessage();
