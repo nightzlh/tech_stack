49fc9636c47ad9d3ac6528f412ebac0841f60664
nightdev <nightdev@163.cm>
2021-02-16 00:34:28 +0800

    finish aop example

--- upper/spring/lab/example-aop/src/main/java/night/dev/App.java
+++ upper/spring/lab/example-aop/src/main/java/night/dev/App.java
@@ -1,5 +1,8 @@
 package night.dev;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 /**
@@ -11,12 +14,23 @@ public class App
     public static void main( String[] args )
     {
 
+
         GenericXmlApplicationContext ctx =
                 new GenericXmlApplicationContext();
-        ctx.load("classpath:spring/app-context-xml.xml");
+        // ctx.load("classpath:spring/app-context-xml.xml");
+        ctx.load("file:/home/nightdev/work/github/tech_stack/upper/spring/lab/example-aop/context.xml");
         ctx.refresh();
         NewDocumentarist documentarist =
                 ctx.getBean("documentarist", NewDocumentarist.class);
         documentarist.execute();
+
+
+        /* GenericApplicationContext ctx =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        NewDocumentarist documentarist =
+        //        ctx.getBean("documentarist", NewDocumentarist.class);
+        documentarist.execute();
+        */
+
     }
 }
