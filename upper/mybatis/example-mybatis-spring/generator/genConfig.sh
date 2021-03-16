2ea93a38cf3d9c93b072b83d2c49a5ed9419648b
zhongliheng <zhongliheng@shoyoo.com>
2021-03-16 16:48:36 +0800

    update mybatisGeneratorConfig

--- upper/mybatis/example-mybatis-spring/generator/genConfig.sh
+++ upper/mybatis/example-mybatis-spring/generator/genConfig.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath ~/.m2/repository/mysql/mysql-connector-java/8.0.22/mysql-connector-java-8.0.22.jar  -jar ~/.m2/repository/org/mybatis/generator/mybatis-generator-core/1.4.0/mybatis-generator-core-1.4.0.jar -configfile ./generatorConfig.xml -overwrite
+java -jar ./lib/mybatis-generator-core-1.4.0.jar -configfile ./generatorConfig.xml -overwrite
